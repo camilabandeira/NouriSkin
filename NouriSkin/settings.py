eeedb925cf8ab0952e6d2b7684bd98831202c80b
Camila <yourname@example.com>
2024-12-07 19:04:34 +0000

    feat: update settings for production, add Procfile, and update dependencies

--- NouriSkin/settings.py
+++ NouriSkin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$=dxp^qntm+=9&8sv!dfr$o*4m+=umix77dx5asmmumiro&l%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '.herokuapp.com', '192.168.0.66']
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Nouriskin.wsgi
